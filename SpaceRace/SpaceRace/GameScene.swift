e18994e179ee1bd88fd4de5f79984021d4bf2a94
Kevia <cloud@kevia.me>
2016-02-09 16:55:38 -0800

    basic mechanics working

--- SpaceRace/SpaceRace/GameScene.swift
+++ SpaceRace/SpaceRace/GameScene.swift
@@ -7,12 +7,52 @@
 //
 
 import SpriteKit
+import GameplayKit
 
-class GameScene: SKScene
+class GameScene: SKScene, SKPhysicsContactDelegate
 {
+    var starfield: SKEmitterNode!
+    var player: SKSpriteNode!
+    var scoreLabel: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    var possibleEnemies = ["ball", "hammer", "tv"]
+    var gameTimer: NSTimer!
+    var gameOver = false
+    
     override func didMoveToView(view: SKView)
     {
+        backgroundColor = UIColor.blackColor()
         
+        starfield = SKEmitterNode(fileNamed: "Starfield")!
+        starfield.position = CGPoint(x: 1024, y: 384)
+        starfield.advanceSimulationTime(10)
+        starfield.zPosition = -1
+        addChild(starfield)
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384)
+        
+        // make player per pixel collision
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody!.contactTestBitMask = 1
+        addChild(player)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .Left
+        addChild(scoreLabel)
+        
+        score = 0
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        physicsWorld.contactDelegate = self
+        
+        gameTimer = NSTimer.scheduledTimerWithTimeInterval(0.35, target: self, selector: "createEnemy", userInfo: nil, repeats: true)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
@@ -22,6 +62,34 @@ class GameScene: SKScene
    
     override func update(currentTime: CFTimeInterval)
     {
+        for node in children
+        {
+            if node.position.x < -300
+            {
+                node.removeFromParent()
+            }
+        }
         
+        if !gameOver
+        {
+            score += 1
+        }
+    }
+    
+    func createEnemy()
+    {
+        possibleEnemies = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(possibleEnemies) as! [String]
+        let randomDistribution = GKRandomDistribution(lowestValue: 50, highestValue: 763)
+        
+        let sprite = SKSpriteNode(imageNamed: possibleEnemies[0])
+        sprite.position = CGPoint(x: 1200, y: randomDistribution.nextInt())
+        addChild(sprite)
+        
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.categoryBitMask = 1
+        sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+        sprite.physicsBody?.angularVelocity = 5
+        sprite.physicsBody?.linearDamping = 0
+        sprite.physicsBody?.angularDamping = 0
     }
 }
